72a59ea8e8f0174f24098a7f2b26e1ffb00ffcf2
yanwhocodes <ynawhocodes@gmail.com>
2022-11-07 01:01:36 +0900

    [chore] #1 typescript setting

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App  = () => {
+ 
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default App;
