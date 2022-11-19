07df3bc229bf20a62a982e473fc20ad332163232
yanwhocodes <ynawhocodes@gmail.com>
2022-11-19 14:25:36 +0900

    [feat] #2 SignInScreen button style 추가

--- src/screens/signIn/SignInScreen.tsx
+++ src/screens/signIn/SignInScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { View, Text, StyleSheet, Pressable } from 'react-native'; 
+import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native'; 
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 type RootStackParamList = {
@@ -10,22 +10,38 @@ type Props = NativeStackScreenProps<RootStackParamList>;
 
 const SignInScreen = ({navigation}: Props) => { 
 	return ( 
-    	<View style={styles.signInScreen}> 
-      <Text>SignInScreen</Text> 
-      <Pressable onPress={()=>navigation.navigate('HomeScreen') }>
-        <Text>
-          SignIn
-        </Text>
+    <View style={styles.signInScreen}> 
+      <TextInput></TextInput>
+      <Pressable onPress={() => navigation.navigate('HomeScreen')} style={styles.btn}>
+        <Text style={styles.btnText}>로그인</Text>
       </Pressable>
-        </View> 
+      <Pressable>
+        <Text style={styles.signUpText}>회원가입</Text>
+      </Pressable>
+    </View> 
     ) 
   } 
 
 const styles = StyleSheet.create({ 
 	signInScreen: { 
-    	flex: 1, 
-        alignItems: 'center', 
-        justifyContent: 'center'  
-    } 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+      
+  },
+  btn: {
+    justifyContent: 'center',
+    width: '80%',
+    height: 50,
+    backgroundColor: '#EE3E36',
+  },
+  btnText: {
+    textAlign: 'center'
+  },
+  signUpText: {
+    textDecorationLine: 'underline',
+    marginVertical: 15,
+  }
 }) 
 export default SignInScreen;
